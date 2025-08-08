ff3992c9d0702ac32db8f1a769b849e0f8084b14
eugen-tkachenko <eugen.n.tkachenko.dev@gmail.com>
2025-08-08 03:43:53 +0200

    Refactor

--- app.php
+++ app.php
@@ -6,9 +6,6 @@ use App\ParserFacade;
 use App\Parser\LogParser;
 use App\DTO\DataDTO;
 use App\Writer\CSVWriter;
-// use App\Enums\FreeProductStatus;
-
-// echo FreeProductStatus::Status0;
 
 (new ParserFacade(new LogParser(), new DataDTO, new CSVWriter))
     ->process('./parser_test/', 'log');
